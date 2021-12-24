09f30921279d4e785cae5a85b93d2636705090d1
jiayixing <1102242300@qq.com>
2021-12-24 10:20:45 +0800

    this is first commit!

---
+++ src/main/java/com/hosday/day01/Test1.java
@@ -0,0 +1,12 @@
+package com.hosday.day01;
+
+/**
+ * @Author XINGXING jia
+ * @Date 2021/12/24 10:18
+ * @Version 1.0
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("my first git");
+    }
+}
