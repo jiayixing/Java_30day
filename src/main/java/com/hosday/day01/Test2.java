5e824333a11971390fd3565ee00fafb17ab04d4e
jiayixing <1102242300@qq.com>
2021-12-24 10:21:07 +0800

    this is first2 commit!

---
+++ src/main/java/com/hosday/day01/Test2.java
@@ -0,0 +1,12 @@
+package com.hosday.day01;
+
+/**
+ * @Author XINGXING jia
+ * @Date 2021/12/24 10:19
+ * @Version 1.0
+ */
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("my sencond");
+    }
+}
