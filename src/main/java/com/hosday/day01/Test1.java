bdf8f634b81bd179d9681979a79430cfca882508
jiayixing <1102242300@qq.com>
2021-12-24 10:39:33 +0800

    3

--- src/main/java/com/hosday/day01/Test1.java
+++ src/main/java/com/hosday/day01/Test1.java
@@ -9,5 +9,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("my first git");
         System.out.println("I am comming");
+        System.out.println("I am comming");
     }
 }
